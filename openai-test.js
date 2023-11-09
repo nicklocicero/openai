8c3cb8428d184c8596fffa05f717283935d17eaf
hannah <hannah@hannahs-MacBook-Pro.local>
2023-11-09 13:12:06 -0800

    updated question and role

--- openai-test.js
+++ openai-test.js
@@ -4,7 +4,7 @@ const openai = new OpenAI();
 
 async function main() {
   const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "You are a helpful assistant." }],
+    messages: [{"role": "user", "content": "Who won the world series in 2020?"}],
     model: "gpt-3.5-turbo",
   });
 
